9cc925104144587ec75606766df08b08ab24ba9b
Metin Kaim <metinkaim0@gmail.com>
2025-12-17 17:07:57 +0300

    Added playablecontroller

---
+++ AppleGrapple/Assets/CoreGame/Scripts/Controllers/PlayableController.cs
@@ -0,0 +1,14 @@
+﻿using Luna.Unity;
+using UnityEngine;
+
+namespace Assets.CoreGame.Scripts.Controllers
+{
+    public class PlayableController : MonoBehaviour
+    {
+        public void GoToStore()
+        {
+            LifeCycle.GameEnded();
+            Playable.InstallFullGame();
+        }
+    }
+}
